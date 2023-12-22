f4ea82219ff5da739bd8f2de01d9b0692b24ef28
erickk1212 <148313041+erickk1212@users.noreply.github.com>
2023-12-22 16:00:28 +0900

    Create 1008.cpp

---
+++ 1000-9999/1008.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	double a,b,c;
+	cin >> a >> b;
+  c=a/b;
+  cout.tie(NULL);
+  cout.precision(10);
+	cout << c << "\n";
+	return 0;
+}
